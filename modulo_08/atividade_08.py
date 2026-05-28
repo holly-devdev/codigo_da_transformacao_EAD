3a36e16a74e7e077e5b32bcb3097f2d3f1b80cd2
alicyapocket <alicyaferreira36@gmail.com>
2026-05-28 14:25:54 -0300

    Create atividade_08.py

---
+++ modulo_08/atividade_08.py
@@ -0,0 +1,23 @@
+'''
+
+atividae na contruçao de um codigo que utliza objetos
+
+
+
+'''
+
+
+
+class Carro:
+    def __init__(self,  Marca, Modelo, cor ):
+        self.Marca = Marca
+        self.Modelo = Modelo
+        self.cor = cor
+        self.ligar = False
+        pass
+
+    
+    
+carro1 = Carro("toyota", "supra", "preto")
+carro1.ligar = ("Ligado")
+print(f'O carro {carro1.Marca} {carro1.Modelo} de cor {carro1.cor} está {carro1.ligar}')
